16691d94d45a883054f72a48626c96b9b947def6
Victor Pothin <victorpothin1@gmail.com>
2020-09-17 09:30:47 -0300

    Update aplicaPCA.m

--- aplicaPCA.m
+++ aplicaPCA.m
@@ -5,15 +5,6 @@ function [score,a,s,coeff] = aplicaPCA(dataBase,variance)
     a=0;
     latent = diag(s);
     
-%     latent = latent/sum(latent);
-%     for i=1:length(latent)
-%         Acumm(i) = sum(latent(1:i));
-%         if Acumm(i)>=(variance/100) && a==0 
-%              a = i;
-%         end
-%     end 
-%     
-
     for i=1:length(latent)
         if latent(i) < 1 && a==0 
              a = i;
